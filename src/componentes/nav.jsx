442dd512443b3b740b34d3d18743a5c537c9f25a
kess1ly <kekezinha2905@gmail.com>
2026-05-21 17:24:19 -0300

    Update nav.jsx

--- src/componentes/nav.jsx
+++ src/componentes/nav.jsx
@@ -2,15 +2,32 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
-      <h1>PetFlow</h1>
+    <nav className="nav">
 
-      <ul>
+      {/* LOGO */}
+      <div className="nav-logo">
+        🐾 PetFlow
+      </div>
+
+      {/* BUSCA (estilo Petz) */}
+      <div className="nav-search">
+        <input type="text" placeholder="Buscar produtos para seu pet..." />
+      </div>
+
+      {/* LINKS */}
+      <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/cliente">Cliente</Link></li>
         <li><Link to="/servicos">Serviços</Link></li>
         <li><Link to="/admin">Admin</Link></li>
       </ul>
+
+      {/* AÇÕES (tipo Petz: usuário/carrinho) */}
+      <div className="nav-actions">
+        <span>👤</span>
+        <span>🛒</span>
+      </div>
+
     </nav>
   );
 }
