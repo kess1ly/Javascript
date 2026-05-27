a725c7e03ed4d941789df6a8e39554b74daac1dd
kess1ly <kekezinha2905@gmail.com>
2026-05-27 11:42:32 -0300

    Update PetItem.jsx

--- src/componentes/PetItem.jsx
+++ src/componentes/PetItem.jsx
@@ -1,15 +1,23 @@
-export default function PetItem({ pet }) {
+export default function PetItem({ pet, removerPet, editarPet, modo }) {
   return (
     <div className="pet-card">
-
       <p><b>Nome:</b> {pet.nome}</p>
+      <p><b>Dono:</b> {pet.dono}</p>
       <p><b>Espécie:</b> {pet.especie}</p>
-      <p><b>Idade:</b> {pet.idade}</p>
+      <p><b>Serviço:</b> {pet.tipo}</p>
+      <p><b>Valor:</b> R$ {pet.valor}</p>
 
-      {pet.observacao && (
-        <p><b>Obs:</b> {pet.observacao}</p>
+      {modo === "admin" && (
+        <div className="pet-actions">
+          <button className="edit-btn" onClick={() => editarPet(pet)}>
+            Editar
+          </button>
+
+          <button className="delete-btn" onClick={() => removerPet(pet.id)}>
+            Excluir
+          </button>
+        </div>
       )}
-
     </div>
   );
 }
