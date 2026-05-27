7cb532316f71e44961c645faaa34b7eba9c5a605
kess1ly <kekezinha2905@gmail.com>
2026-05-27 11:43:02 -0300

    Update PetList.jsx

--- src/componentes/PetList.jsx
+++ src/componentes/PetList.jsx
@@ -1,10 +1,16 @@
 import PetItem from "./PetItem";
 
-export default function PetList({ pets }) {
+export default function PetList({ pets, removerPet, editarPet, modo }) {
   return (
     <div className="pet-list">
       {pets.map((pet) => (
-        <PetItem key={pet.id} pet={pet} />
+        <PetItem
+          key={pet.id}
+          pet={pet}
+          removerPet={removerPet}
+          editarPet={editarPet}
+          modo={modo}
+        />
       ))}
     </div>
   );
