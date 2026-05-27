5c39df43771b27c98567ee6ab7fe2a6add9fb5b8
kess1ly <kekezinha2905@gmail.com>
2026-05-27 11:41:05 -0300

    Update Home.jsx

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -3,41 +3,34 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home">
+      <h1>Bem-vindo ao PetFlow 🐾</h1>
 
-      {/* HERO */}
-      <section className="home-hero">
-        <h1>Bem-vindo ao PetFlow 🐾</h1>
+      <p>
+        Sistema de gerenciamento para pet shops.
+        Controle clientes, pets, serviços e atendimentos.
+      </p>
 
-        <p>
-          Sistema completo para pet shops.
-          Gerencie pets, clientes, serviços e atendimentos de forma simples.
-        </p>
+      <div className="home-cards">
 
-        <Link to="/cliente" className="haome-ct">
-          Começar agora
-        </Link>
-      </section>
-
-      {/* CARDS */}
-      <section className="home-cards">
-
+        {/* CLIENTE CADASTRA PETS */}
         <Link to="/cliente" className="home-card">
-          <h3>🐶 Cadastro de Pets</h3>
+          <h3>Cadastro de Pets</h3>
           <p>Clientes podem cadastrar e visualizar seus pets.</p>
         </Link>
 
+        {/* SERVIÇOS */}
         <Link to="/servicos" className="home-card">
-          <h3>🛁 Serviços</h3>
+          <h3>Serviços</h3>
           <p>Banho, tosa, consulta e vacinação.</p>
         </Link>
 
+        {/* ADMIN */}
         <Link to="/admin" className="home-card">
-          <h3>⚙️ Painel Admin</h3>
-          <p>Gerencie pets, clientes e atendimentos.</p>
+          <h3>Painel Admin</h3>
+          <p>Editar, remover e gerenciar todos os pets.</p>
         </Link>
 
-      </section>
-
+      </div>
     </div>
   );
 }
